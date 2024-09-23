64c1a8fa0b0c34ec1ed57814136cc369d1ea5a1c
DanielDarwiche <danielinti1@hotmail.com>
2024-09-23 11:14:45 +0200

    v4 fixes och innehåll

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,6 +14,7 @@ const Skills: React.FC = () => {
   <li className="liskills">React</li>
   <li className="liskills">Bootstrap</li>
   <li className="liskills">Mantine</li>
+  <li className="liskills">SPA</li>
 </ul>
 <h3 style={{ color: '#00204a' }}>Back end:</h3>
 <ul className='grid'>
@@ -21,17 +22,24 @@ const Skills: React.FC = () => {
   <li className="liskills">.NET</li>
   <li className="liskills">ASP.NET</li>
   <li className="liskills">APIs</li>
-  <li className="liskills">SSMS</li>
+  <li className="liskills">MinimalAPIs</li>
+  <li className="liskills">MSSQL</li>
   <li className="liskills">Node.js</li>
   <li className="liskills">WebForms</li>
   <li className="liskills">Dapper</li>
   <li className="liskills">Entity Framework</li>
   <li className="liskills">Git</li>
+  <li className="liskills">LINQ</li>
+  <li className="liskills">MVC</li>
   <li className="liskills">AzureDevOps</li>
 </ul>
 <h3 style={{ color: '#00204a' }}>Other skills:</h3>
 <ul className='grid'>
   <li className="liskills">OOP</li>
+  <li className="liskills">TDD</li>
+  <li className="liskills">xUnit</li>
+  <li className="liskills">nUnit</li>
+  <li className="liskills">MSTest</li>
   <li className="liskills">Agile</li>
   <li className="liskills">Scrum</li>
   <li className="liskills">English</li>
